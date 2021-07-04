bc8e6e7683b93edec64a2e4db4d87c714e062f73
ageratinaaltissima <said.olgun@boun.edu.tr>
2021-07-04 21:12:29 +0300

    create new category

---
+++ CET322Final/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using CET322Final.DataBase;
+using CET322Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CET322Final.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<CAtegory> objList = _db.Category;
+            return View(objList);
+        }
+        //Get Create
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CAtegory obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
